13bf673a4d9dd4f30e3672a7f00a9fd567316996
Philipp Hochkamp <ragonnullx3@gmail.com>
2019-01-15 18:57:04 +0100

    d
    XC

--- geoparser.py
+++ geoparser.py
@@ -40,7 +40,7 @@ def logparse(LOGPATH, INFLUXHOST, INFLUXPORT, INFLUXDBDB, INFLUXUSER, INFLUXUSER
                 time.sleep(1)
                 FILE.seek(WHERE)
             else:
-              if "\n" in LINE:  
+              if "\s" in LINE:  
                 IP = LINE.split("\s")[1]
                 if IP:
                     INFO = GI.record_by_addr(IP)
