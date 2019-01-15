8fb3f16a1eda815c921711c3b69bd10b3b01c450
Philipp Hochkamp <philipp.hochkamp@tu-dortmund.de>
2019-01-15 18:31:33 +0100

    adfadfaa

--- geoparser.py
+++ geoparser.py
@@ -40,7 +40,8 @@ def logparse(LOGPATH, INFLUXHOST, INFLUXPORT, INFLUXDBDB, INFLUXUSER, INFLUXUSER
                 time.sleep(1)
                 FILE.seek(WHERE)
             else:
-                IP = re.search(GETIP, LINE).group(1)
+              if LINE:  
+                IP = LINE.split("\s")[1]
                 if IP:
                     INFO = GI.record_by_addr(IP)
                     if INFO is not None:
